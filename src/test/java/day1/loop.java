690ad6cfef72f6b116b2ddf59cbe9d1da2d16f67
Emrah Bulut <bltemrah@gmail.com>
2022-07-30 19:19:20 +0100

    day 1

---
+++ src/test/java/day1/loop.java
@@ -0,0 +1,28 @@
+package day1;
+
+public class loop {
+
+    public static void main(String[] args) {
+
+//        do
+
+        int n = 1;
+        do {
+            System.out.println("It runs at least one time");
+            n++;
+        }
+        while (n<5);
+
+//        while
+
+        int i = 0;
+
+        while (i<5) {
+            System.out.println("Java");
+            i++;
+        }
+
+
+    }
+
+}
---
+++ src/test/java/day1/switchCase.java
@@ -0,0 +1,61 @@
+package day1;
+
+import java.util.Scanner;
+
+public class switchCase {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int number = scan.nextInt();
+
+        switch (number) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            default:
+                System.out.println("Gecersiz sayi");
+                break;
+        }
+
+//        Balance
+
+        int balance = 1000;
+        System.out.println("1: Display balance");
+        System.out.println("2: Deposit");
+        System.out.println("3: Withdraw");
+        System.out.println("4: Exit");
+
+        int select = scan.nextInt();
+
+        switch (select) {
+            case 1:
+                System.out.println("Your balance is: " + balance);
+                break;
+            case 2:
+                System.out.println("How much deposit");
+                int dAmount = scan.nextInt();
+                balance+=dAmount;
+                System.out.println("Your balance is: " + balance);
+                break;
+            case 3:
+                System.out.println("Withdraw amount");
+                int wAmount = scan.nextInt();
+                balance-=wAmount;
+                System.out.println("Your balance is: " + balance);
+                break;
+            case 4:
+                System.out.println("Have a nice day");
+                break;
+            default:
+                System.out.println("Not Applicaple");
+        }
+    }
+}
