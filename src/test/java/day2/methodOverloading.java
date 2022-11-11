f1d23ac52d74584c3bf2601c9e242cc492f8c21d
Emrah Bulut <e.bulut@hymnai.com>
2022-11-11 10:02:15 +0000

    interview questions

--- src/test/java/day2/methodOverloading.java
+++ src/test/java/day2/methodOverloading.java
@@ -6,19 +6,21 @@ public class methodOverloading {
 
         System.out.println(sum( 3, 5));
         System.out.println(sum( 3, 5, 9));
+        word("word");
     }
 
     public static int sum(int a, int b) {
 
         return (a + b);
-
-
     }
 
     public static int sum(int a, int b, int c) {
 
         return (a + b + c);
-
     }
 
+    public static void word(String str) {
+
+        System.out.println(str);
+    }
 }
